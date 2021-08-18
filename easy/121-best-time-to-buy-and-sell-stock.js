209dae35fb987942591418bffb16d7e8c205376a
Philip Lowe <philip.lowe94@gmail.com>
2021-08-18 14:40:06 -0400

    started solving 121

---
+++ easy/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    
+    let minprice = prices[0];
+    let maxprofit = 0;
+    
+    for(let i = 0; i < prices.length - 1; i++) {
+        if(prices[i] < minprice) {
+            minprice = prices[i];
+        }
+        let profit = prices[i] - minprice;
+        if(profit > maxprofit) {
+            maxprofit = profit;
+        }
+    }
+    
+    return maxprofit;
+};
