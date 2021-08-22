e7c7838b264c924544e29394fce0368d41fca6bf
Philip Lowe <philip.lowe94@gmail.com>
2021-08-22 19:56:10 -0400

    complete leetcode 7

--- easy/7-reverse-integer.js
+++ easy/7-reverse-integer.js
@@ -5,10 +5,15 @@
  var reverse = function(x) {
     
     let reversed = "";
-
     let num;
+    let neg = false;
+     
+    if(x < 0) {
+        x *= -1;
+        neg = true;
+    }
 
-    while ( x > 10 ) {
+    while ( x >= 10 ) {
         num = x % 10;
         reversed += `${num}`;
         x = parseInt(x/10);
@@ -16,5 +21,14 @@
 
     reversed += `${x}`;
 
-    return eval(reversed);
+    x = parseInt(reversed);
+    if(neg) {
+        x *= -1;
+    }
+     
+    if ((x > 2**31 - 1) || (x < -(2**31))) {
+        return 0;
+    }
+     
+    return x;
 };
