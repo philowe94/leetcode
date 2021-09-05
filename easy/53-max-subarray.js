a800b2e2ae2447490a6c0bd3b47b3e2ef2290144
Philip Lowe <philip.lowe94@gmail.com>
2021-09-04 23:11:43 -0400

    problem 53 completed

--- easy/53-max-subarray.js
+++ easy/53-max-subarray.js
@@ -4,17 +4,26 @@
  */
  var maxSubArray = function(nums) {
     
-    let sum = 0;
-    let startnum = nums[0];
+    let runningSum = nums[0];
+    let largestSum = nums[0];
+    let startingNum = nums[0];
     
-    for(let i = 0; i < nums.length; i++) {
-        sum += nums[i];
-        if (sum < startnum) {
-            startnum = nums[i];
-            sum = 0;
-        }
+    for(let i = 1; i < nums.length; i++) {
+        runningSum = Math.max(runningSum + nums[i], nums[i]);
+        largestSum = Math.max(runningSum, largestSum)
+        
     }
     
-    return sum;
+    return largestSum;
     
 };
+
+/*
+sum= 0
+startnum = -2
+
+sum=-2
+
+
+
+*/
