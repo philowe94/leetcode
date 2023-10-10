1ac5bd3345d3704a3f013c9144a3f95c9a44fd14
Philip Lowe <philip.lowe94@gmail.com>
2023-10-09 23:17:33 -0400

    climbing stairs

--- easy/509-fibonacci-number.py
+++ easy/509-fibonacci-number.py
@@ -1,11 +1,11 @@
 import pdb
 
 class Solution:
-    def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        breakpoint()
-        return self.fib(n-1) + self.fib(n-2)
+	def fib(self, n: int) -> int:
+		if n <= 1:
+			return n
+		breakpoint()
+		return self.fib(n-1) + self.fib(n-2)
 
 s = Solution()    
 
