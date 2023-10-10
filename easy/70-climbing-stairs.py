6b381b28f569de0138a037edfcf84b8cfdbb6c3d
Philip Lowe <philip.lowe94@gmail.com>
2023-10-09 23:27:42 -0400

    better climbing stairs soln

--- easy/70-climbing-stairs.py
+++ easy/70-climbing-stairs.py
@@ -2,12 +2,18 @@ import pdb
 
 class Solution:
 	def climbStairs(self, n: int) -> int:
-		one, two = 1,1
+		one, two = 2,1
 
-		for i in range(n-1):
+		for i in range(n-2):
 			temp = one
 			one = one + two
 			two = temp
 			
 		return one
 		
+# 5 step staircase
+# from step 4, there is one way
+# from step 3, there is two ways
+# from step 2, there is three ways
+# from step 1, there is five ways
+# from step 0, there is eight ways
